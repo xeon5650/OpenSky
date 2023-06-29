7f8b23447339d9d0980e56a06dfb82dde5a9f31f
Vladimir Zabolotskikh <m30b34735@gmail.com>
2023-06-30 00:47:26 +0300

    Add docstrings

--- OpenSky.py
+++ OpenSky.py
@@ -1,3 +1,4 @@
+"""Module for interacting with OpenSky"""
 import logging
 import datetime
 from datetime import datetime
@@ -7,21 +8,40 @@ import pandas as pd
 
 
 class FlightData:
+    """Class for store flightData"""
+
     def __get_iata_by_icao(self, icao):
+        """
+        Search IATA code by ICAO
+        :param icao: specified airport ICAO code
+        :return: airport IATA code
+        """
         coordinates = self._airports
         try:
             iata = coordinates[coordinates['ICAO'] == icao]['IATA'].tolist()[0]
             return iata
-        except Exception as e:
+        except KeyError:
             print("IATA not found")
             return None
 
     def __unix_to_iso(self, unix_time):
+        """
+        Conver DateTime in unix to time in ISO
+        :param unix_time: DateTime in Unix format
+        :return: time in ISO format %Y-%m-%dT%H:%M:%SZ
+        """
+
         iso_time = datetime.fromtimestamp(unix_time).strftime('%Y-%m-%dT%H:%M:%SZ')
 
         return iso_time
 
     def __calc_distance(self, dep_icao, arr_icao):
+        """
+        Calc distance between airports by their ICAO codes
+        :param dep_icao: Airport icao code
+        :param arr_icao: Airport icao code
+        :return: distance in Km
+        """
         coordinates = self._airports
         try:
             from_lat = coordinates[coordinates['ICAO'] == dep_icao]['Latitude'].tolist()[0]
@@ -33,11 +53,17 @@ class FlightData:
                           (round(float(to_lat), 3), round(float(to_lon), 3))).km
             distance = round(float(distance), 3)
             return distance
-        except Exception as e:
+        except KeyError:
             print("Airports coordinates not found")
             return None
 
+    __slots__ = ['_airports', '_flights']
+
     def __init__(self, flights_json):
+        """
+        Initialize Flight data object
+        :param flights_json: json with flight info
+        """
         self._airports = pd.read_csv('world_airports.csv', names=['Airport ID',
                                                                   'Name',
                                                                   'City',
@@ -59,107 +85,130 @@ class FlightData:
             dep_icao = flight[1]['estDepartureAirport']
             arr_icao = flight[1]['estArrivalAirport']
             distance = self.__calc_distance(dep_icao, arr_icao)
-            flights_data.DepartureAirport_ICAO = dep_icao
-            flights_data.ArrivalAirport_ICAO = arr_icao
-            flights_data.DepartureTime = self.__unix_to_iso(flight[1]['firstSeen'])
-            flights_data.ArrivalTime = self.__unix_to_iso(flight[1]['lastSeen'])
-            flights_data.DISTANCE = distance
-            flights_data.DepartureAirport_IATA = self.__get_iata_by_icao(dep_icao)
-            flights_data.ArrivalAirport_IATA = self.__get_iata_by_icao(arr_icao)
-            flights_data.Callsign = flight[1]['callsign']
+            flights_data.departure_airport_icao = dep_icao
+            flights_data.arrival_airport_icao = arr_icao
+            flights_data.departure_time = self.__unix_to_iso(flight[1]['firstSeen'])
+            flights_data.arrival_time = self.__unix_to_iso(flight[1]['lastSeen'])
+            flights_data.distance = distance
+            flights_data.departure_airport_iata = self.__get_iata_by_icao(dep_icao)
+            flights_data.arrival_airport_iata = self.__get_iata_by_icao(arr_icao)
+            flights_data.call_sign = flight[1]['callsign']
             self[flight[0]] = flights_data
 
     def __len__(self):
+        """
+        :return: number of elements in FlightData
+        """
         return len(self._flights)
 
     def __setitem__(self, flight_number, flight_object):
+        """
+        set FlightObject in FlightData by specified index
+        :param flight_number: specified index
+        :param flight_object: FlightObject
+        :return: FlightObject
+        """
         self._flights[flight_number] = flight_object
 
     def __getitem__(self, flight_number):
+        """
+        Return FlightObject with specified index from FlightData
+        :param flight_number: specified index
+        :return: FlightObject
+        """
         return self._flights[flight_number]
 
     def to_dict(self):
+        """
+        Convert FlightData to dictionary
+        :return: dict
+        """
 
-        dict = {'DepartureAirport_ICAO': [entry.DepartureAirport_ICAO for entry in self],
-                'ArrivalAirport_ICAO': [entry.ArrivalAirport_ICAO for entry in self],
-                'DepartureTime': [entry.DepartureTime for entry in self],
-                'ArrivalTime': [entry.ArrivalTime for entry in self],
-                'DISTANCE': [entry.DISTANCE for entry in self],
-                'DepartureAirport_IATA': [entry.DepartureAirport_IATA for entry in self],
-                'ArrivalAirport_IATA': [entry.ArrivalAirport_IATA for entry in self],
-                'Callsign': [entry.Callsign for entry in self],
-                }
-        return dict
+        dictionary = {'DepartureAirport_ICAO': [entry.departure_airport_icao for entry in self],
+                      'ArrivalAirport_ICAO': [entry.arrival_airport_icao for entry in self],
+                      'DepartureTime': [entry.departure_time for entry in self],
+                      'ArrivalTime': [entry.arrival_time for entry in self],
+                      'DISTANCE': [entry.distance for entry in self],
+                      'DepartureAirport_IATA': [entry.departure_airport_iata for entry in self],
+                      'ArrivalAirport_IATA': [entry.arrival_airport_iata for entry in self],
+                      'Callsign': [entry.call_sign for entry in self],
+                      }
+        return dictionary
 
     def to_pandas_df(self):
+        """
+        Convert FlightData to Pandas DataFrame
+        :return: Pandas DataFrame
+        """
         return pd.DataFrame(self.to_dict())
 
     class FlightObject:
-        __slots__ = ["DepartureAirport_ICAO", "ArrivalAirport_ICAO",
-                     "DepartureTime", "ArrivalTime",
-                     "DISTANCE", "DepartureAirport_IATA",
-                     "ArrivalAirport_IATA", "Callsign"]
+        __slots__ = ["departure_airport_icao", "arrival_airport_icao",
+                     "departure_time", "arrival_time",
+                     "distance", "departure_airport_iata",
+                     "arrival_airport_iata", "call_sign"]
 
-        def __init__(self, DepartureAirport_ICAO=None, ArrivalAirport_ICAO=None,
-                     DepartureTime=None, ArrivalTime=None, DISTANCE=None, DepartureAirport_IATA=None,
-                     ArrivalAirport_IATA=None, Callsign=None):
-            self.DepartureAirport_ICAO = DepartureAirport_ICAO
-            self.ArrivalAirport_ICAO = ArrivalAirport_ICAO
-            self.DepartureTime = DepartureTime
-            self.ArrivalTime = ArrivalTime
-            self.DISTANCE = DISTANCE
-            self.DepartureAirport_IATA = DepartureAirport_IATA
-            self.ArrivalAirport_IATA = ArrivalAirport_IATA
-            self.Callsign = Callsign
+        def __init__(self, departure_airport_icao=None, arrival_airport_icao=None,
+                     departure_time=None, arrival_time=None, distance=None,
+                     departure_airport_iata=None, arrival_airport_iata=None,
+                     call_sign=None):
+            self.departure_airport_icao = departure_airport_icao
+            self.arrival_airport_icao = arrival_airport_icao
+            self.departure_time = departure_time
+            self.arrival_time = arrival_time
+            self.distance = distance
+            self.departure_airport_iata = departure_airport_iata
+            self.arrival_airport_iata = arrival_airport_iata
+            self.call_sign = call_sign
 
         def __repr__(self):
-            attrib_value = {"DepartureAirport_ICAO": self.DepartureAirport_ICAO,
-                            "ArrivalAirport_ICAO": self.ArrivalAirport_ICAO,
-                            "DepartureTime": self.DepartureTime,
-                            "ArrivalTime": self.ArrivalTime,
-                            "DISTANCE": self.DISTANCE,
-                            "DepartureAirport_IATA": self.DepartureAirport_IATA,
-                            "ArrivalAirport_IATA": self.ArrivalAirport_IATA,
-                            "Callsign": self.Callsign}
+            attrib_value = {"DepartureAirport_ICAO": self.departure_airport_icao,
+                            "ArrivalAirport_ICAO": self.arrival_airport_icao,
+                            "DepartureTime": self.departure_time,
+                            "ArrivalTime": self.arrival_time,
+                            "DISTANCE": self.distance,
+                            "DepartureAirport_IATA": self.departure_airport_iata,
+                            "ArrivalAirport_IATA": self.arrival_airport_iata,
+                            "Callsign": self.call_sign}
             return str(attrib_value)
 
-        def __dict__(self):
-            attrib_value = {"DepartureAirport_ICAO": [self.DepartureAirport_ICAO],
-                            "ArrivalAirport_ICAO": [self.ArrivalAirport_ICAO],
-                            "DepartureTime": [self.DepartureTime],
-                            "ArrivalTime": [self.ArrivalTime],
-                            "DISTANCE": [self.DISTANCE],
-                            "DepartureAirport_IATA": [self.DepartureAirport_IATA],
-                            "ArrivalAirport_IATA": [self.ArrivalAirport_IATA],
-                            "Callsign": [self.Callsign]}
+        def to_dict(self):
+            attrib_value = {"departure_airport_icao": [self.departure_airport_icao],
+                            "arrival_airport_icao": [self.arrival_airport_icao],
+                            "departure_time": [self.departure_time],
+                            "arrival_time": [self.arrival_time],
+                            "distance": [self.distance],
+                            "departure_airport_iata": [self.departure_airport_iata],
+                            "arrival_airport_iata": [self.arrival_airport_iata],
+                            "call_sign": [self.call_sign]}
             return attrib_value
 
         def __str__(self):
-            attrib_value = {f"DepartureAirport_ICAO - {self.DepartureAirport_ICAO}",
-                            f"ArrivalAirport_ICAO - {self.ArrivalAirport_ICAO}",
-                            f"DepartureTime - {self.DepartureTime}",
-                            f"ArrivalTime - {self.ArrivalTime}",
-                            f"DISTANCE - {self.DISTANCE}",
-                            f"DepartureAirport_IATA - {self.DepartureAirport_IATA}",
-                            f"ArrivalAirport_IATA - {self.ArrivalAirport_IATA}",
-                            f"Callsign {self.Callsign}"}
+            attrib_value = {f"departure_airport_icao - {self.departure_airport_icao}",
+                            f"arrival_airport_icao - {self.arrival_airport_icao}",
+                            f"departure_time - {self.departure_time}",
+                            f"arrival_time - {self.arrival_time}",
+                            f"distance - {self.distance}",
+                            f"departure_airport_iata - {self.departure_airport_iata}",
+                            f"arrival_airport_iata - {self.arrival_airport_iata}",
+                            f"call_sign {self.call_sign}"}
             return str(attrib_value)
 
         def to_pandas_df(self):
-            return pd.DataFrame(self.__dict__())
+            return pd.DataFrame(self.to_dict())
 
 
-class OpenSkyApi(object):
+class OpenSkyApi:
     __slots__ = ['_auth', '__sess', '__base_url', '_airports']
 
-
-    def __iso_to_unix(self,date_iso):
+    def __iso_to_unix(self, date_iso):
         date_format = datetime.strptime(date_iso,
-                                                 "%Y-%m-%dT%H:%M:%SZ")
+                                        "%Y-%m-%dT%H:%M:%SZ")
         unix_time = int(datetime.timestamp(date_format))
         return unix_time
 
     def __init__(self, login=None, password=None):
+        logging.basicConfig(level=logging.DEBUG, format='%(message)s')
         self._auth = (login, password)
         self.__sess = requests.Session()
         self.__base_url: str = 'https://opensky-network.org/api'
@@ -180,24 +229,23 @@ class OpenSkyApi(object):
                                                                   ])
 
     def __get_flights_json(self, operation, params):
-        r = self.__sess.get(
+        response = self.__sess.get(
             f"{self.__base_url}{operation}",
             auth=self._auth,
             params=params
         )
-        if r.status_code == 200:
-            return r.json()
+        if response.status_code == 200:
+            output = response.json()
 
         else:
             logging.error(
-                f"Request for {params} "
-                f"Error occurred: {r.status_code}"
+                "Error occurred:%s", response.status_code
             )
-        return None
+            output = None
+        return output
 
     def get_arrivals_by_airport(self, airport, start, end, json_output=True):
         start = self.__iso_to_unix(start)
-        print(start)
         end = self.__iso_to_unix(end)
         if start >= end:
             raise ValueError("The end daytame must be greater than start.")
@@ -208,11 +256,12 @@ class OpenSkyApi(object):
         flights_json = self.__get_flights_json(
             "/flights/arrival", params=params
         )
-        flights_obj = FlightData(flights_json)
+
         if json_output:
-            return flights_json
+            output = flights_json
         else:
-            return flights_obj
+            output = FlightData(flights_json)
+        return output
 
     def get_departures_by_airport(self, airport, start, end, json_output=True):
         start = self.__iso_to_unix(start)
@@ -227,8 +276,9 @@ class OpenSkyApi(object):
         flights_json = self.__get_flights_json(
             "/flights/departure", params=params
         )
-        flights_obj = FlightData(flights_json)
+
         if json_output:
-            return flights_json
+            output = flights_json
         else:
-            return flights_obj
+            output = FlightData(flights_json)
+        return output
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import OpenSky
 
 api = OpenSky.OpenSkyApi('Zabolotskikh','TT7.622023.')
-p = api.get_departures_by_airport('KLAX','2023-06-15T00:00:00Z','2023-06-22T00:00:00Z',False)
+p = api.get_departures_by_airport('KPSP','2023-06-15T00:00:00Z','2023-06-22T00:00:00Z',False)
+
 p.to_pandas_df().to_csv('TEST.csv')
-print(p.to_pandas_df())
+print(p.to_dict())
 
